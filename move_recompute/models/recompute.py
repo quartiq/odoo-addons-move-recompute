a03147108653b90627d741bcb8c780575eb19bef
Robert Jördens <rj@quartiq.de>
2023-05-15 14:17:23 +0200

    kwargs

--- move_recompute/models/recompute.py
+++ move_recompute/models/recompute.py
@@ -5,6 +5,7 @@ from odoo import api, models
 class MoveRecompute(models.Model):
     _inherit = 'account.move'
 
-    def recompute_dynamic_lines(self, **kwargs):
-        super(MoveRecompute, self)._recompute_dynamic_lines(**kwargs)
+    def recompute_dynamic_lines(self, recompute_all_taxes=False,
+                                recompute_tax_base_amount=False):
+        super(MoveRecompute, self)._recompute_dynamic_lines(recompute_all_taxes, recompute_tax_base_amount)
         return True
